200941b197653bc5c4da2b38a96135642e61c8c2
Philipp Klose <me@thehippo.de>
2021-02-01 21:26:34 +0100

    add information on latest release to the popup.html

--- ext/popup.js
+++ ext/popup.js
@@ -1,1 +1,25 @@
-document.getElementById('version_installed').innerText = chrome.runtime.getManifest().version;
+document.getElementById('version-installed').innerText = chrome.runtime.getManifest().version;
+
+fetch('https://api.github.com/repos/TheHippo/mmplus/releases')
+	.then(res => res.json())
+	.then((releases) => {
+		console.log(releases)
+		for (release of releases) {
+			console.dir(release)
+			if (release.draft || release.prerelease) {
+				continue;
+			}
+			document.getElementById('version-available').innerText = release.tag_name.slice(1);
+			for (asset of release.assets) {
+				if (asset.name == 'mmplus.zip') {
+					document.getElementById('download-button').setAttribute('href', asset.browser_download_url);
+					break;
+				}
+			}
+			break;
+		}
+	}).catch(err => {
+		console.error(err)
+		document.getElementById('version-available').innerText = 'Could not find newest release';
+		document.getElementById('download-button').remove();
+	});
